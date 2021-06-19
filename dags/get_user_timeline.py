2bc28dfc1a4b233cac5519764cf80ea6e8a6ad46
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-06-19 16:07:33 +0430

    Add username list

--- dags/get_user_timeline.py
+++ dags/get_user_timeline.py
@@ -18,20 +18,20 @@ def get_api_object():
     return api
 
 
-def get_last_tweet_id(conn_id):
+def get_last_tweet_id(conn_id, username):
     hook = MongoHook(conn_id=conn_id)
 
-    marker = hook.find(mongo_collection='marker', query={"importer_key": "last_tweet_id"}, find_one=True, mongo_db='test')
+    marker = hook.find(mongo_collection='marker', query={"importer_key": f"last_tweet_id_{username}"}, find_one=True, mongo_db='test')
     if marker:
         return marker['last_id']
     return
 
 
-def first_time_getting_tweets(conn_id, api):
+def first_time_getting_tweets(conn_id, api, username):
 
     alltweets = []
 
-    new_tweets = api.user_timeline(screen_name='mstootfarangi', count=300)
+    new_tweets = api.user_timeline(screen_name=username, count=300)
 
     alltweets.extend(new_tweets)
 
@@ -39,13 +39,13 @@ def first_time_getting_tweets(conn_id, api):
     if len(alltweets) > 0:
         last_id = alltweets[-1].id - 1
     else:
-        print("The username you've provided has not any tweet so far.")
+        print(f"The username you've provided ({username}) has not any tweet so far.")
         return
 
     #keep grabbing tweets until there are no tweets left to grab
     while (len(new_tweets) > 0) :
 
-        new_tweets = api.user_timeline(screen_name = 'mstootfarangi', count=300, max_id=last_id)
+        new_tweets = api.user_timeline(screen_name = username, count=300, max_id=last_id)
 
         print(f"getting tweets before {last_id}")
 
@@ -63,19 +63,19 @@ def first_time_getting_tweets(conn_id, api):
         hook = MongoHook(conn_id=conn_id)
         hook.insert_one(                                                                            
             mongo_collection='marker',                                                                       
-            doc={"importer_key": "last_tweet_id", "last_id": latest_id},                                                       
+            doc={"importer_key": f"last_tweet_id_{username}", "last_id": latest_id},
             mongo_db='test'                                                                              
         )
     outtweets = [{'tw_id':tweet.id_str, 'tw_created_at': tweet.created_at, 'tw_text': tweet.text, 'tw_user': tweet.author._json['screen_name']} for tweet in alltweets]
     return outtweets
     
 
-def get_tweets(last_id, conn_id, api):
+def get_tweets(last_id, conn_id, api, username):
 
     alltweets = []
     
     #make initial request for most recent tweets greater than an id
-    new_tweets = api.user_timeline(screen_name='mstootfarangi', count=300, since_id=last_id)
+    new_tweets = api.user_timeline(screen_name=username, count=300, since_id=last_id)
 
     print(f"getting tweets after {last_id}")
     
@@ -87,13 +87,13 @@ def get_tweets(last_id, conn_id, api):
     if alltweets and len(alltweets) > 0:
         last_id = alltweets[0].id + 1
     else:
-        print("The username you've provided has not any new tweet.")
+        print(f"The username you've provided ({username}) has not any new tweet.")
         return
 
     hook = MongoHook(conn_id=conn_id)
     hook.update_one(
         mongo_collection='marker',                                                                       
-        filter_doc={"importer_key": "last_tweet_id"},
+        filter_doc={"importer_key": f"last_tweet_id_{username}"},
         update_doc={"$set": {'last_id': last_id}},
         mongo_db='test'                                                                              
     )
@@ -105,23 +105,24 @@ def insert_to_mongo(**kwargs):
     conn_id = kwargs['conn_id']
     hook = MongoHook(conn_id)
     api = get_api_object()
-
-    last_id = get_last_tweet_id(conn_id)
-    outtweets = []
-    if not last_id:
-        outtweets = first_time_getting_tweets(conn_id, api)
-    else:
-        outtweets = get_tweets(last_id, conn_id, api)
-    print(f'All tweets are {outtweets}')
-    if outtweets and len(outtweets)>0:
+    usernames = ['mstootfarangi', 'kimia_azim']
+    for username in usernames:
+        print(f'Import tweets for {username}')
+        last_id = get_last_tweet_id(conn_id, username)
+        if not last_id:
+            outtweets = first_time_getting_tweets(conn_id, api, username)
+        else:
+            outtweets = get_tweets(last_id, conn_id, api, username)
+        print(f'All tweets are {outtweets}')
+        if outtweets and len(outtweets)>0:
             hook.insert_many(
             mongo_collection='twitter',
             docs=outtweets,
             mongo_db='test'
         )
 
-    new_importer_key = hook.find(mongo_collection='marker', query={"importer_key": "last_tweet_id"}, find_one=True, mongo_db='test')
-    print(f'The new importer key is {new_importer_key}')
+        new_importer_key = hook.find(mongo_collection='marker', query={"importer_key": f"last_tweet_id_{username}"}, find_one=True, mongo_db='test')
+        print(f'The new importer key for {username} is {new_importer_key}')
 
 """Default arguments used on DAG parameters."""
 default_args = {
