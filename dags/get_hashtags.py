2420644d9771bca7be6f3755794c1cfbb5f7fcf6
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-07-20 15:43:11 +0430

    make tidy, decrease retires

--- dags/get_hashtags.py
+++ dags/get_hashtags.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.contrib.hooks.mongo_hook import MongoHook
@@ -22,8 +22,10 @@ def get_api_object():
 def get_latest_tweet_time(conn_id, hashtag):
     hook = MongoHook(conn_id=conn_id)
 
-    marker = hook.find(mongo_collection='marker', query={"importer_key": f"latest_tweet_time_{hashtag}"}, find_one=True,
-                       mongo_db='test')
+    marker = hook.find(
+        mongo_collection='marker', query={'importer_key': f'latest_tweet_time_{hashtag}'}, find_one=True,
+        mongo_db='test',
+    )
     if marker:
         return marker['last_time']
     return
@@ -31,84 +33,91 @@ def get_latest_tweet_time(conn_id, hashtag):
 
 def getting_hashtags(conn_id, hashtags, **kwargs):
     api = get_api_object()
-    alltweets = []
+
+    all_tweets = []
     last_hashtag_index = 0
+
     for hashtag in hashtags:
         latest_created_at = get_latest_tweet_time(conn_id, hashtag)
         print(f'The last tweet time for {hashtag} is {latest_created_at}')
+
         if not latest_created_at:
             new_tweets = tweepy.Cursor(api.search, q=hashtag).items(750)
         else:
             new_tweets = tweepy.Cursor(api.search, q=hashtag, since=latest_created_at).items(750)
 
-        alltweets.extend(new_tweets)
-        len_all_tweets = len(alltweets)
+        all_tweets.extend(new_tweets)
+        len_all_tweets = len(all_tweets)
+        # Get count of tweets after each call, since we keep them in one array
         if len_all_tweets > 0:
             print(f'Number of new tweets is {len_all_tweets - last_hashtag_index + 1}')
-            latest_tweet_time = alltweets[last_hashtag_index].created_at
+            latest_tweet_time = all_tweets[last_hashtag_index].created_at
             # Saving the index of the earliest tweet of a hashtag
             last_hashtag_index = len_all_tweets + 1
         else:
             print(f"The hashtag you've provided ({hashtag}) has not any new tweet so far.")
             continue
 
-        print(f"...{len_all_tweets} tweets downloaded so far")
+        print(f'...{len_all_tweets} tweets downloaded so far')
 
+        # Update marker
         if latest_tweet_time:
             hook = MongoHook(conn_id=conn_id)
             hook.update_one(
                 mongo_collection='marker',
-                filter_doc={"importer_key": f"latest_tweet_time_{hashtag}"},
-                update_doc={"$set": {'last_time': latest_tweet_time.strftime('%Y-%m-%d')}},
+                filter_doc={'importer_key': f'latest_tweet_time_{hashtag}'},
+                update_doc={'$set': {'last_time': latest_tweet_time.strftime('%Y-%m-%d')}},
                 upsert=True,
-                mongo_db='test'
+                mongo_db='test',
             )
-    outtweets = [{'tw_hashtags': tweet.entities['hashtags'], 'tw_id': tweet.id_str, 'tw_created_at': tweet.created_at,
-                  'tw_text': tweet.text, 'tw_user': tweet.author._json['screen_name'],
-                  'tw_location': tweet.author._json['location']} for tweet in alltweets]
+    out_tweets = [
+        {
+            'tw_hashtags': tweet.entities['hashtags'], 'tw_id': tweet.id_str, 'tw_created_at': tweet.created_at,
+            'tw_text': tweet.text, 'tw_user': tweet.author._json['screen_name'],
+            'tw_location': tweet.author._json['location'],
+        } for tweet in all_tweets
+    ]
 
-    kwargs['task_instance'].xcom_push(key='all_tweets', value=outtweets)
+    kwargs['task_instance'].xcom_push(key='all_tweets', value=out_tweets)
 
 
 def insert_to_mongo(**kwargs):
     conn_id = kwargs['conn_id']
-    outtweets = kwargs['task_instance'].xcom_pull(task_ids='get_hashtags', key='all_tweets')
+    out_tweets = kwargs['task_instance'].xcom_pull(task_ids='get_hashtags', key='all_tweets')
     hook = MongoHook(conn_id)
-    filter_docs = [{'tw_id': doc['tw_id']} for doc in outtweets]
-    if outtweets and len(outtweets) > 0:
-        a = hook.replace_many(
+    if out_tweets and len(out_tweets) > 0:
+        result = hook.insert_many(
             mongo_collection='twitter',
-            docs=outtweets,
-            filter_docs=filter_docs,
+            docs=out_tweets,
             mongo_db='test',
-            upsert=True
         )
+        print(f'Tweets have been inserted into Mongo. {result}')
 
-"""Default arguments used on DAG parameters."""
+
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
     'start_date': days_ago(2),
-    'retries': 480,
+    'retries': 10,
     'retry_delay': timedelta(minutes=30),
 }
 
 dag_params = {
     'dag_id': 'mongo_hashtags',
     'default_args': default_args,
-    'schedule_interval': '@daily',
+    'schedule_interval': '0 23 * * *',
     'catchup': False,
     'max_active_runs': 1,
 }
-"""Default parameters used on DAG instance."""
+
 with DAG(**dag_params) as dag:
+
     get_hashtags = PythonOperator(
         task_id='get_hashtags',
         python_callable=getting_hashtags,
         provide_context=True,
         op_kwargs={
             'conn_id': 'mongo_default',
-            'hashtags': ['#unitedAIRLINES', '#Flight', '#beach']
+            'hashtags': ['#unitedAIRLINES', '#Flight', '#beach'],
         },
         dag=dag,
     )
@@ -124,4 +133,3 @@ with DAG(**dag_params) as dag:
     )
 
     get_hashtags >> insert_rows
-
--- dags/get_user_timeline.py
+++ dags/get_user_timeline.py
@@ -1,10 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.contrib.hooks.mongo_hook import MongoHook
 from airflow.operators.python_operator import PythonOperator
 
 import tweepy
+
 
 def get_api_object():
     consumer_key = 'consumer_key'
@@ -21,115 +22,127 @@ def get_api_object():
 def get_last_tweet_id(conn_id, username):
     hook = MongoHook(conn_id=conn_id)
 
-    marker = hook.find(mongo_collection='marker', query={"importer_key": f"last_tweet_id_{username}"}, find_one=True, mongo_db='test')
+    marker = hook.find(
+        mongo_collection='marker', query={'importer_key': f'last_tweet_id_{username}'}, find_one=True,
+        mongo_db='test',
+    )
     if marker:
         return marker['last_id']
     return
 
 
 def first_time_getting_tweets(conn_id, api, username):
-
-    alltweets = []
+    all_tweets = []
 
     new_tweets = api.user_timeline(screen_name=username, count=300)
 
-    alltweets.extend(new_tweets)
+    all_tweets.extend(new_tweets)
 
-   #save the id of the oldest tweet less one
-    if len(alltweets) > 0:
-        last_id = alltweets[-1].id - 1
+    # save the id of the oldest tweet less one
+    if len(all_tweets) > 0:
+        last_id = all_tweets[-1].id - 1
     else:
         print(f"The username you've provided ({username}) has not any tweet so far.")
         return
 
-    #keep grabbing tweets until there are no tweets left to grab
-    while (len(new_tweets) > 0) :
+    # keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        new_tweets = api.user_timeline(screen_name=username, count=300, max_id=last_id)
 
-        new_tweets = api.user_timeline(screen_name = username, count=300, max_id=last_id)
+        print(f'getting tweets before {last_id}')
 
-        print(f"getting tweets before {last_id}")
+        # save most recent tweets
+        all_tweets.extend(new_tweets)
 
-        #save most recent tweets
-        alltweets.extend(new_tweets)
+        # update the id of the oldest tweet less one
+        last_id = all_tweets[-1].id - 1
 
-        #update the id of the oldest tweet less one
-        last_id = alltweets[-1].id - 1
+        print(f'...{len(all_tweets)} tweets downloaded so far')
 
-        print(f"...{len(alltweets)} tweets downloaded so far")
-
-    latest_id = alltweets[0].id + 1
+    latest_id = all_tweets[0].id + 1
 
     if latest_id:
         hook = MongoHook(conn_id=conn_id)
-        hook.insert_one(                                                                            
-            mongo_collection='marker',                                                                       
-            doc={"importer_key": f"last_tweet_id_{username}", "last_id": latest_id},
-            mongo_db='test'                                                                              
+        hook.insert_one(
+            mongo_collection='marker',
+            doc={'importer_key': f'last_tweet_id_{username}', 'last_id': latest_id},
+            mongo_db='test',
         )
-    outtweets = [{'tw_id':tweet.id_str, 'tw_created_at': tweet.created_at, 'tw_text': tweet.text.encode('utf-8'), 'tw_user': tweet.author._json['screen_name']} for tweet in alltweets]
-    return outtweets
-    
+    out_tweets = [
+        {
+            'tw_id': tweet.id_str, 'tw_created_at': tweet.created_at, 'tw_text': tweet.text.encode('utf-8'),
+            'tw_user': tweet.author._json['screen_name'],
+        } for tweet in all_tweets
+    ]
+    return out_tweets
+
 
 def get_tweets(last_id, conn_id, api, username):
+    all_tweets = []
 
-    alltweets = []
-    
-    #make initial request for most recent tweets greater than an id
+    # make initial request for most recent tweets greater than an id
     new_tweets = api.user_timeline(screen_name=username, count=300, since_id=last_id)
 
-    print(f"getting tweets after {last_id}")
-    
-    alltweets.extend(new_tweets)
+    print(f'getting tweets after {last_id}')
 
-    print(f"...{len(alltweets)} tweets downloaded so far")
+    all_tweets.extend(new_tweets)
 
-   #save the id of the oldest tweet less one
-    if alltweets and len(alltweets) > 0:
-        last_id = alltweets[0].id + 1
+    print(f'...{len(all_tweets)} tweets downloaded so far')
+
+    # save the id of the oldest tweet less one
+    if all_tweets and len(all_tweets) > 0:
+        last_id = all_tweets[0].id + 1
     else:
         print(f"The username you've provided ({username}) has not any new tweet.")
         return
 
     hook = MongoHook(conn_id=conn_id)
     hook.update_one(
-        mongo_collection='marker',                                                                       
-        filter_doc={"importer_key": f"last_tweet_id_{username}"},
-        update_doc={"$set": {'last_id': last_id}},
-        mongo_db='test'                                                                              
+        mongo_collection='marker',
+        filter_doc={'importer_key': f'last_tweet_id_{username}'},
+        update_doc={'$set': {'last_id': last_id}},
+        mongo_db='test',
     )
-    outtweets = [{'tw_id': tweet.id_str, 'tw_created_at': tweet.created_at, 'tw_text': tweet.text.encode('utf-8'), 'tw_user': tweet.author._json['screen_name']} for tweet in alltweets]
-    return outtweets
+    out_tweets = [
+        {
+            'tw_id': tweet.id_str, 'tw_created_at': tweet.created_at, 'tw_text': tweet.text.encode('utf-8'),
+            'tw_user': tweet.author._json['screen_name'],
+        } for tweet in all_tweets
+    ]
+    return out_tweets
 
 
 def insert_to_mongo(**kwargs):
     conn_id = kwargs['conn_id']
     hook = MongoHook(conn_id)
     api = get_api_object()
-    usernames = ['mstootfarangi', 'kimia_azim']
+    usernames = ['ApacheAirflow', 'Airbnb']
     for username in usernames:
         print(f'Import tweets for {username}')
         last_id = get_last_tweet_id(conn_id, username)
         if not last_id:
-            outtweets = first_time_getting_tweets(conn_id, api, username)
+            out_tweets = first_time_getting_tweets(conn_id, api, username)
         else:
-            outtweets = get_tweets(last_id, conn_id, api, username)
-        print(f'All tweets are {outtweets}')
-        if outtweets and len(outtweets)>0:
+            out_tweets = get_tweets(last_id, conn_id, api, username)
+        print(f'All tweets are {out_tweets}')
+        if out_tweets and len(out_tweets) > 0:
             hook.insert_many(
-            mongo_collection='twitter',
-            docs=outtweets,
-            mongo_db='test'
-        )
+                mongo_collection='twitter',
+                docs=out_tweets,
+                mongo_db='test',
+            )
 
-        new_importer_key = hook.find(mongo_collection='marker', query={"importer_key": f"last_tweet_id_{username}"}, find_one=True, mongo_db='test')
+        new_importer_key = hook.find(
+            mongo_collection='marker', query={'importer_key': f'last_tweet_id_{username}'},
+            find_one=True, mongo_db='test',
+        )
         print(f'The new importer key for {username} is {new_importer_key}')
 
-"""Default arguments used on DAG parameters."""
+
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
     'start_date': days_ago(2),
-    'retries': 480,
+    'retries': 10,
     'retry_delay': timedelta(minutes=30),
 }
 
@@ -140,14 +153,13 @@ dag_params = {
     'catchup': False,
     'max_active_runs': 1,
 }
-"""Default parameters used on DAG instance."""
+
 with DAG(**dag_params) as dag:
     insert_row = PythonOperator(
         task_id='insert_to_mongo',
         python_callable=insert_to_mongo,
         op_kwargs={
-            'conn_id': 'mongo_default'
+            'conn_id': 'mongo_default',
         },
         dag=dag,
     )
-
